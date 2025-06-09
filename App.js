4f8566dd7662837bf4282ffcd5372ac1b39ba755
atulyaaj <atulyaaj6@gmail.com>
2025-06-10 03:22:04 +0530

    day-02

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *          <h1>I am h1 tag</h1>
- *          <h2>I am h2 tag</h2>
- *      </div>
- *      <div id="child2">
- *          <h1>I am h1 tag</h1>
- *          <h2>I am h2 tag</h2>
- *      </div>
- * </div>
- *
- *
- * ReactElement(Object) => HTML(Browser Understands)
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "This is Atulya!"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -30,12 +15,7 @@ const parent = React.createElement("div", { id: "parent" }, [
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
-console.log(parent); // React Element(Object)
-
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+console.log(parent);
 
 
 
