2a48a54ac90fe9c0ddd210df2cc2e3ac9a2aeb2f
atulyaaj <atulyaaj6@gmail.com>
2025-06-13 12:24:23 +0530

    day-03-01

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Atulya!"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+// React Element
+const heading = (
+  <h1 className="head" tabIndex="5">
+    React using JSX
+  </h1>
+);
+
+console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-console.log(parent);
-
-
-
-
-
+root.render(heading);
