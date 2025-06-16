695a6627c3516f9e25cfcf1529d467ac28150330
atulyaaj <atulyaaj6@gmail.com>
2025-06-16 11:20:09 +0530

    day-03-02

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = (
+const elem = <span>React Element</span>;
+
+const title = (
   <h1 className="head" tabIndex="5">
+    {elem}
     React using JSX
   </h1>
 );
 
-console.log(heading);
+const Title = () => {
+  return (
+    <h1 className="head" tabIndex="5">
+      React using JSX
+    </h1>
+  );
+};
+
+// Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    {title}
+    <h1 className="heading">React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
+
+// root.render(title);
